14613969bcd7c0253fc67990b6898adb5f46e756
YadlaMani <yadlamaniymn2005@gmail.com>
2025-07-13 13:46:27 +0530

    middleware changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,18 +6,31 @@ const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
   limiter: Ratelimit.slidingWindow(10, "1 m"),
 });
+
 export async function middleware(req: NextRequest) {
   const forwarded = req.headers.get("x-forwarded-for");
   const ip = forwarded?.split(",")[0] || "";
+
   const { success } = await ratelimit.limit(ip);
+
   if (success) {
     return NextResponse.next();
   }
-  return NextResponse.json({
-    status: 429,
-    message: "Too many requests, please try again later.",
-  });
+
+  return new NextResponse(
+    JSON.stringify({
+      status: 429,
+      message: "Too many requests, please try again later.",
+    }),
+    {
+      status: 429,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 }
+
 export const config = {
   matcher: ["/api/leetcode/:path*", "/api/codechef/:path*"],
 };
